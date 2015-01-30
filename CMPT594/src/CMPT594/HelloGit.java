e7ca987191d6bc8517dc3c0e8a9c869719a5ac67
Cici <cuicuiruancs@gmail.com>
2015-01-29 20:44:34 -0500

    test

--- CMPT594/src/CMPT594/HelloGit.java
+++ CMPT594/src/CMPT594/HelloGit.java
@@ -5,6 +5,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Kevin here!");
 		System.out.println("Kevin's work PC here!");
+		System.out.printlin("Cici is here");
 	}
 
 }
