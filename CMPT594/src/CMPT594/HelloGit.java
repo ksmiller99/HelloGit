7652e3e26a5c944b3f6931194aa99b7331cab8e3
Kevin S. Miller <ksm07003-spam@yahoo.com>
2015-01-29 21:52:24 -0500

    Changed laoptop line

--- CMPT594/src/CMPT594/HelloGit.java
+++ CMPT594/src/CMPT594/HelloGit.java
@@ -3,7 +3,7 @@ package CMPT594;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("Kevin here!");
+		System.out.println("Kevin's laptop here!");
 		System.out.println("Kevin's work PC here!");
 		System.out.println("Cici is here");
 		System.out.println("Andrew too.");
