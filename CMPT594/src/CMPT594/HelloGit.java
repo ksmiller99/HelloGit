356ae2870bc462ed52d71a5ada3557e1e919f6f8
Kevin S. Miller <ksm07003-spam@yahoo.com>
2015-01-29 20:47:30 -0500

    Test 2

--- CMPT594/src/CMPT594/HelloGit.java
+++ CMPT594/src/CMPT594/HelloGit.java
@@ -5,7 +5,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Kevin here!");
 		System.out.println("Kevin's work PC here!");
-		System.out.printlin("Cici is here");
+		System.out.println("Cici is here");
 	}
 
 }
